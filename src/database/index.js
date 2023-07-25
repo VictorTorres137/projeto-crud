4a0118bb1022f676e972e0d8fa599a2359dff10a
Victor Torres <vdst2011@hotmail.com>
2023-07-24 22:06:07 -0300

    Resolvendo Problema no Mongoose

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose')
 
 
+
+const db = mongoose.connection
 
 function conect(){
 // mongoose.set('useNewUrlParser', true)
     
     mongoose.connect('mongodb://127.0.0.1:27017/projeto-crud')
     
-    const db = mongoose.connection
     
-    db.once('open', () =>{
+    db.once('open', () => {
         console.log('Conected to database')
     })
     
@@ -21,24 +21,3 @@ function conect(){
     module.exports = {
         conect
     }
-=======
-
-const mongoose = require('mongoose')
-
-function connect(){
-
-  mongoose.connect('mongodb://127.0.0.1:27017/projeto-crud')
-  
-  const db = mongoose.connection
-  
-  db.once('open', () => {
-    console.log('Connected to database!')
-  })
-  
-  db.on('error', console.error.bind(console, 'conection error:'))
-}
-
-module.exports = {
-  connect,
-}
->>>>>>> 87d32b75ee1967eea2b2c942c4b5b5c22c869134
--- src/models/customers.js
+++ src/models/customers.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
@@ -6,22 +5,8 @@ const schema = new mongoose.Schema({
     age: Number,
     email: String,
     password: String,
-=======
-const mongoose = require ('mongoose')
-
-const schema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  email:String,
-  password:String,
-
->>>>>>> 87d32b75ee1967eea2b2c942c4b5b5c22c869134
 })
 
 const Model = mongoose.model('customers', schema)
 
-<<<<<<< HEAD
 module.exports = Model
-=======
-module.exports = Model
->>>>>>> 87d32b75ee1967eea2b2c942c4b5b5c22c869134
